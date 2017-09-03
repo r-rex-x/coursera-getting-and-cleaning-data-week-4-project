6349b0e83920a4d01b8c47d8abe519f3953d0bce
rex <rhcwong@gmail.com>
2017-09-03 16:25:44 +0800

    final

--- run_analysis.R
+++ run_analysis.R
@@ -9,40 +9,49 @@ run_analysis <- function() {
         X_train <- read.table("train/X_train.txt")
         y_train <- read.table("train/y_train.txt")
         
-        # Name the variables
-        features <- read.table("features.txt")
-        names(X_test) <- features[,2]
-        names(X_train) <- features[,2]
-        
-        # Match variables with subject ID & activity labels
-        names(subject_test) <- "ID"
-        names(subject_train) <- "ID"
-        names(y_test) <- "activity"
-        names(y_train) <- "activity"
-        
         testdata <- data.frame(subject_test, y_test, X_test)
         traindata <- data.frame(subject_train, y_train, X_train)
+        
         
         # 1.
         # Combine test and train data into one dataset
         alldata <- rbind(traindata, testdata)
         
+        
+        
         # 2.
         # Find out which variables are measurements on mean or standard deviation
-        colIndex <- which(grepl("mean()", names(alldata)) | grepl("std()", names(alldata)))
+        features <- read.table("features.txt")
+        features[ ,2] <- as.character(features[ ,2])
+        colIndex <- grep(".*mean.*|.*std.*", features[ ,2])
         
         # Extracts only the measurements on the mean and standard deviation
-        mydata <- alldata[, c(1, 2, colIndex)]
+        mydata <- alldata[, c(1, 2, colIndex+2)]
+        
+        
         
         # 3.
         # Uses descriptive activity names to name the activities in the data set
         activities <- read.table("activity_labels.txt")
-        mydata$activity <- factor(mydata$activity, levels = activities[ ,1], labels = activities[ ,2])
+        mydata[ ,2] <- factor(mydata[ ,2], levels = activities[ ,1], labels = activities[ ,2])
+        
+      
         
         # 4.
-        # All varibles in the data set are already labelled with descriptive names 
-        # when matched with ID and activity before combining into one dataset.
-
+        # Name all the variables with descriptive names
+        
+        # Read the variable names from features list
+        descriptive.names <- c("ID", "activity", features[colIndex ,2])
+        
+        # Clean up - & () in variable names
+        descriptive.names <- gsub("-mean", "Mean", descriptive.names)
+        descriptive.names <- gsub("-std", "Std", descriptive.names)
+        descriptive.names <- gsub("[()]", "", descriptive.names)
+        descriptive.names <- gsub("[-]", "", descriptive.names)
+        
+        names(mydata) <- descriptive.names
+        
+        
         # 5.
         # Create a second, independent tidy data set with the avearge of each vairiable 
         # for each activity and each subject ID
@@ -50,5 +59,5 @@ run_analysis <- function() {
         tidydataset <- ddply(mydata, .(ID, activity), function(x) {colMeans(x[ ,3:81])})
         
         # Write the tidy set into a file.
-        write.table(tidydataset, file = "tidydataset.txt", row.names=FALSE)
+        write.table(tidydataset, file = "tidydataset.txt", row.names = FALSE, quote = FALSE)
 }
